61e2ce131aec1dc7b8aa788b6c9c7fc61840cda4
Chris Cameron <chrisjamescameron1@gmail.com>
2026-09-10 09:13:08 +1200

    fix(cpp): the MPGOS hooks are empty; the timed kernel is the integration alone

--- GPU_ODE_MPGOS/problems/stubs.cuh
+++ GPU_ODE_MPGOS/problems/stubs.cuh
@@ -1,8 +1,7 @@
 #ifndef PROBLEM_STUBS_CUH
 #define PROBLEM_STUBS_CUH
 
-// MPGOS hooks; sPARi[0] is the run budget in 2^21-cycle units, ACCi[0] the thread's start clock.
-#define WATCHDOG_CLOCK_SHIFT 21
+// MPGOS hooks; every one is empty, so the timed kernel is the integration alone.
 
 template <class Precision>
 __forceinline__ __device__ void PerThread_EventFunction(\
@@ -28,12 +27,7 @@ __forceinline__ __device__ void PerThread_ActionAfterSuccessfulTimeStep(\
 			Precision&    T, Precision&   dT, Precision*    TD, Precision*   X, \
 			Precision* cPAR, Precision* sPAR, int*       sPARi, Precision* ACC, int* ACCi)
 {
-	if ((int)(clock64() >> WATCHDOG_CLOCK_SHIFT) - ACCi[0] > sPARi[0])
-	{
-		for (int c = 0; c < PROBLEM_SD; c++)
-			X[c] = (Precision)nan("");
-		UDT = 1;
-	}
+
 }
 
 template <class Precision>
@@ -42,7 +36,7 @@ __forceinline__ __device__ void PerThread_Initialization(\
 			Precision&    T, Precision&   dT, Precision*    TD, Precision*   X, \
 			Precision* cPAR, Precision* sPAR,       int* sPARi, Precision* ACC, int* ACCi)
 {
-	ACCi[0] = (int)(clock64() >> WATCHDOG_CLOCK_SHIFT);
+
 }
 
 template <class Precision>
